72890f79aa51f6eb1e56899db9347821371b9645
Daniel Elebash <delebash@users.noreply.github.com>
2023-01-20 20:19:32 -0500

    init

---
+++ Source/WaterBlueprintFunctionLibrary/Private/WaterBlueprintFunctionLibraryBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WaterBlueprintFunctionLibraryBPLibrary.h"
+#include "WaterBlueprintFunctionLibrary.h"
+
+UWaterBlueprintFunctionLibraryBPLibrary::UWaterBlueprintFunctionLibraryBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UWaterBlueprintFunctionLibraryBPLibrary::WaterBlueprintFunctionLibrarySampleFunction(float Param)
+{
+	return -1;
+}
+
